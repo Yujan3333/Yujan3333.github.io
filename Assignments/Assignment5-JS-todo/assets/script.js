7bbcb84cd61169c175ef10d635ba94a24c927b0c
Yujan Basnet <yujanbasnet18@gmail.com>
2023-11-29 10:07:15 +0545

    JS1 todo update

--- Assignments/Assignment5-JS-todo/assets/script.js
+++ Assignments/Assignment5-JS-todo/assets/script.js
@@ -15,9 +15,9 @@ addTaskSubmit.addEventListener("click", function (event) {
 const addTask = () => {
   const taskText = addTaskInput.value.trim();
   if (taskText !== "") {
-    const oneTask= {
-      content:taskText,
-      status:false
+    const oneTask = {
+      content: taskText,
+      status: false,
     };
 
     // push the new added task to the array
@@ -29,43 +29,64 @@ const addTask = () => {
     // clearing the text field of input
     addTaskInput.value = "";
   }
-}
+};
 
 // function to update the task to determine whether its completed or not
-const updateTasks= () =>{
-
+const updateTasks = () => {
   //clear the current lists
-  listItem.innerHTML="";
-  completedItem.innerHTML="";
+  listItem.innerHTML = "";
+  completedItem.innerHTML = "";
 
-  allTasks.forEach((oneTask,index) => {
+  if (allTasks.length === 0) {
+    // If there are no tasks, display a "List is empty" item with different content for list and completed list
+    createEmptyListItem(listItem, "No tasks to be done");
+    createEmptyListItem(completedItem, "No completed tasks");
+  } else {
+    allTasks.forEach((oneTask, index) => {
+      // For each list li
+      const li = document.createElement("li");
+      // 1st element is checkbox
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = oneTask.status;
+      checkbox.onchange = () => toggleTaskCompletion(index);
+      li.appendChild(checkbox);
 
-    // For each list li
-    const li = document.createElement("li");
-    // 1st element is checkbox
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = oneTask.status;
-        checkbox.onchange = () => toggleTaskCompletion(index);
-        li.appendChild(checkbox);
+      // 2nd element is span for inline
+      const textSpan = document.createElement("span");
+      textSpan.innerText = oneTask.content;
+      li.appendChild(textSpan);
 
-        // 2nd element is span for inline
-        const textSpan = document.createElement("span");
-        textSpan.innerText = oneTask.content;
-        li.appendChild(textSpan);
-
-        // condition to put in completed block or add new task block
-        if(oneTask.status){
-          li.classList.add("completed")
-          completedItem.appendChild(li);
-        } else{
-          listItem.appendChild(li);
-        }
+      // condition to put in completed block or add new task block
+      if (oneTask.status) {
+        li.classList.add(
+          "completed",
+          "list-group-item",
+          "d-flex",
+          "justify-content-start",
+          "align-items-center"
+        );
+        completedItem.appendChild(li);
+      } else {
+        li.classList.add(
+          "list-group-item",
+          "d-flex",
+          "justify-content-start",
+          "align-items-center"
+        );
+        listItem.appendChild(li);
+      }
+    });
   }
-  )
+};
 
+// Function to create a "List is empty" item with custom content
+const createEmptyListItem = (list, content) => {
+  const li = document.createElement("li");
+  li.classList.add('p-3','list-group-item', 'd-flex', 'justify-content-start', 'align-items-center');
+  li.innerText = content;
+  list.appendChild(li);
 }
-
 
 // Function to toggle task completion
 function toggleTaskCompletion(index) {
@@ -75,4 +96,3 @@ function toggleTaskCompletion(index) {
 
 // for initial phase
 updateTasks();
-
